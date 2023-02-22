302fe87259112567cd55c808a9e674f256c15411
Will Adamson <william.adamson@roq.co.uk>
2023-02-22 11:21:37 +0000

    First Push

--- src/test/java/com/credersi/animals/CatTest.java
+++ src/test/java/com/credersi/animals/CatTest.java
@@ -2,6 +2,7 @@ package com.credersi.animals;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
 
@@ -12,4 +13,38 @@ public class CatTest {
 		assertTrue(cat.isAlive());
 		assertEquals(cat.howHungry(), 50);
 	}
+	
+	@Test
+	public void testNineDeathRuns() {
+		Cat cat = new Cat();
+		for (int i = 0; i < 9; i++) {
+			cat.runs();
+			cat.runs();
+		}
+		assertFalse(cat.isAlive());
+	}
+	
+	@Test
+	public void testEightDeathRuns() {
+		Cat cat = new Cat();
+		for (int i = 0; i < 8; i++) {
+			cat.runs();
+			cat.runs();
+		}
+		assertTrue(cat.isAlive());
+	}
+	
+	@Test
+	public void testTaxidermy() {
+		Cat cat = new Cat();
+		for (int i = 0; i < 9; i++) {
+			cat.runs();
+			cat.runs();
+		}
+		cat.feeds();
+		assertEquals(cat.howHungry(), 110);
+		assertFalse(cat.isAlive());
+	}
+	// Feeding shouldn't affect hunger when the cat is dead.
+
 }
